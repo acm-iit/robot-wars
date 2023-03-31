baba809adb36e6b2630002ac12b9fe71783cfe03
Tommy Vadakumchery <t.vadakumchery1@gmail.com>
2023-03-31 10:18:41 -0500

    Add stress test map

---
+++ generate_stress_test.py
@@ -0,0 +1,26 @@
+import json
+from typing import List
+
+from map import MapJson, PositionJson
+
+WIDTH = 2048
+HEIGHT = 2048
+
+SPAWNS_PER_DIMENSION = 8
+
+spawns: List[PositionJson] = [{
+    "x": WIDTH // SPAWNS_PER_DIMENSION * x + WIDTH // SPAWNS_PER_DIMENSION // 2,
+    "y": HEIGHT // SPAWNS_PER_DIMENSION * y + HEIGHT // SPAWNS_PER_DIMENSION // 2
+} for x in range(SPAWNS_PER_DIMENSION) for y in range(SPAWNS_PER_DIMENSION)]
+
+map: MapJson = {
+    "size": {
+        "width": WIDTH,
+        "height": HEIGHT
+    },
+    "walls": [],
+    "spawns": spawns
+}
+
+with open("stress_test.json", "w") as file:
+    json.dump(map, file, indent=4)
