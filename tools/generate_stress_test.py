80419f6321ebef17ccfa35dc028202ccdb46348a
Tommy Vadakumchery <t.vadakumchery1@gmail.com>
2023-04-03 18:57:56 -0500

    Address linting issues in generate_stress_test.py

--- tools/generate_stress_test.py
+++ tools/generate_stress_test.py
@@ -9,7 +9,7 @@ import sys
 
 sys.path.append(".")
 
-from engine.map import MapJson, PositionJson
+from engine.map import MapJson, PositionJson    # noqa: E402
 
 FILENAME = "engine/maps/stress_test.json"
 
@@ -19,10 +19,16 @@ HEIGHT = 2048
 SPAWNS_PER_DIMENSION = 8
 
 if __name__ == "__main__":
-    spawns: list[PositionJson] = [{
-        "x": WIDTH // SPAWNS_PER_DIMENSION * x + WIDTH // SPAWNS_PER_DIMENSION // 2,
-        "y": HEIGHT // SPAWNS_PER_DIMENSION * y + HEIGHT // SPAWNS_PER_DIMENSION // 2
-    } for x in range(SPAWNS_PER_DIMENSION) for y in range(SPAWNS_PER_DIMENSION)]
+    spawn_width = WIDTH / SPAWNS_PER_DIMENSION
+    spawn_height = HEIGHT / SPAWNS_PER_DIMENSION
+    spawns: list[PositionJson] = [
+        {
+            "x": spawn_width * (x + 0.5),
+            "y": spawn_height * (y + 0.5)
+        }
+        for x in range(SPAWNS_PER_DIMENSION)
+        for y in range(SPAWNS_PER_DIMENSION)
+    ]
 
     map: MapJson = {
         "size": {
