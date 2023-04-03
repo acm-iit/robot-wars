a93a67c2ea4cad3d243b217a753d1ff4af729168
Tommy Vadakumchery <t.vadakumchery1@gmail.com>
2023-04-03 18:08:34 -0500

    Wrap generate_stress_test.py in __main__

--- tools/generate_stress_test.py
+++ tools/generate_stress_test.py
@@ -18,19 +18,20 @@ HEIGHT = 2048
 
 SPAWNS_PER_DIMENSION = 8
 
-spawns: list[PositionJson] = [{
-    "x": WIDTH // SPAWNS_PER_DIMENSION * x + WIDTH // SPAWNS_PER_DIMENSION // 2,
-    "y": HEIGHT // SPAWNS_PER_DIMENSION * y + HEIGHT // SPAWNS_PER_DIMENSION // 2
-} for x in range(SPAWNS_PER_DIMENSION) for y in range(SPAWNS_PER_DIMENSION)]
+if __name__ == "__main__":
+    spawns: list[PositionJson] = [{
+        "x": WIDTH // SPAWNS_PER_DIMENSION * x + WIDTH // SPAWNS_PER_DIMENSION // 2,
+        "y": HEIGHT // SPAWNS_PER_DIMENSION * y + HEIGHT // SPAWNS_PER_DIMENSION // 2
+    } for x in range(SPAWNS_PER_DIMENSION) for y in range(SPAWNS_PER_DIMENSION)]
 
-map: MapJson = {
-    "size": {
-        "width": WIDTH,
-        "height": HEIGHT
-    },
-    "walls": [],
-    "spawns": spawns
-}
+    map: MapJson = {
+        "size": {
+            "width": WIDTH,
+            "height": HEIGHT
+        },
+        "walls": [],
+        "spawns": spawns
+    }
 
-with open(FILENAME, "w") as file:
-    json.dump(map, file, indent=4)
+    with open(FILENAME, "w") as file:
+        json.dump(map, file, indent=4)
