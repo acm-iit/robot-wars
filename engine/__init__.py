5f9da0a1b3ca66815472c861b1f6aca337f04d08
Tommy Vadakumchery <t.vadakumchery1@gmail.com>
2023-04-02 19:28:28 -0500

    Hide pygame prompt

--- engine/__init__.py
+++ engine/__init__.py
@@ -1,3 +1,6 @@
+import os
+os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"
+
 from engine.arena import Arena as Arena
 from engine.entity import Bullet as Bullet
 from engine.entity import Entity as Entity
