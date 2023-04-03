9739b3a0ff17d828d6f9a6e5fede6bf0654c1365
Tommy Vadakumchery <t.vadakumchery1@gmail.com>
2023-04-03 18:45:22 -0500

    Address init.py linting issues

--- init.py
+++ init.py
@@ -1,20 +1,31 @@
-import pygame
+"""
+Testing script which loads a map, creates a controllable robot, creates several
+NPC robots, and simulates.
+"""
 from random import random
+
+import pygame
 
 from engine import Arena, Robot
 
+
 def keyboard_control(robot: Robot):
+    """Keyboard control scheme for the user-controlled Robot."""
     keys = pygame.key.get_pressed()
     robot.move_power = -1 if keys[pygame.K_s] else 1 if keys[pygame.K_w] else 0
     robot.turn_power = -1 if keys[pygame.K_a] else 1 if keys[pygame.K_d] else 0
-    robot.turret_turn_power = -1 if keys[pygame.K_q] else 1 if keys[pygame.K_e] else 0
+    robot.turret_turn_power = -1 if keys[pygame.K_q] else 1 \
+        if keys[pygame.K_e] else 0
     if keys[pygame.K_SPACE]:
         robot.shoot()
 
+
 def create_spin_control():
+    """Creates random control scheme for an NPC Robot."""
     move_power = random() * 2 - 1
     turn_power = random() * 2 - 1
     turret_turn_power = random() * 2 - 1
+
     def spin_control(robot: Robot):
         robot.shoot()
         robot.move_power = move_power
@@ -22,6 +33,7 @@ def create_spin_control():
         robot.turret_turn_power = turret_turn_power
 
     return spin_control
+
 
 if __name__ == "__main__":
     arena = Arena.from_map_json("engine/maps/stress_test.json")
@@ -44,7 +56,7 @@ if __name__ == "__main__":
 
     arena.spawn_entities(robots)
 
-    #arena.show_hitboxes = True
-    #arena.show_fps = True
-    #arena.show_quadtree = True
+    # arena.show_hitboxes = True
+    # arena.show_fps = True
+    # arena.show_quadtree = True
     arena.run()
