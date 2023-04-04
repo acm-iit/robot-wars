2250fdcbbfea4a3fcd5b6a1c701080cf12826009
Tommy Vadakumchery <t.vadakumchery1@gmail.com>
2023-04-03 19:54:31 -0500

    Address linting issues in engine/__init__.py

--- engine/__init__.py
+++ engine/__init__.py
@@ -1,8 +1,8 @@
 import os
 os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"
 
-from engine.arena import Arena as Arena
-from engine.entity import Bullet as Bullet
-from engine.entity import Entity as Entity
-from engine.entity import Robot as Robot
-from engine.entity import Wall as Wall
+from engine.arena import Arena as Arena         # noqa
+from engine.entity import Bullet as Bullet      # noqa
+from engine.entity import Entity as Entity      # noqa
+from engine.entity import Robot as Robot        # noqa
+from engine.entity import Wall as Wall          # noqa
