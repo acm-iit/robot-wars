820339bb6a3161af95630b00b3e63e27698f57da
Tommy Vadakumchery <t.vadakumchery1@gmail.com>
2023-04-03 20:05:48 -0500

    Address linting issues in engine/util/__init__.py

--- engine/util/__init__.py
+++ engine/util/__init__.py
@@ -1,5 +1,6 @@
+# flake8: noqa
 from engine.util.draw import draw_gradient as draw_gradient
 from engine.util.draw import draw_gradient_line as draw_gradient_line
-from engine.util.draw import draw_gradient_path as draw_gradient_path
+from engine.util.draw import draw_gradient_path as draw_gradient_path                               
 from engine.util.geometry import check_polygon_collision as check_polygon_collision
 from engine.util.geometry import get_minimum_translation_vector as get_minimum_translation_vector
