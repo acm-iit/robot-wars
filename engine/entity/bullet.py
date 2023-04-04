42b6784129cb1db45e37d4f13a56c2c3e8ff2b43
Tommy Vadakumchery <t.vadakumchery1@gmail.com>
2023-04-04 00:55:59 -0500

    Address linting issues in engine/entity/bullet.py

--- engine/entity/bullet.py
+++ engine/entity/bullet.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 import math
+
 import pygame
 
 import engine.entity as entity
@@ -16,12 +17,15 @@ TRAIL_TAIL_COLOR = "#AAAAAA00"
 
 NUM_HITBOX_VERTICES = 3
 
+
 class Bullet(entity.Entity):
-    def __init__(self, position: Vector2, rotation: float, origin: entity.Robot):
+    def __init__(self, position: Vector2, rotation: float,
+                 origin: entity.Robot):
         super().__init__()
-        self.__lifetime = 2                     # Lifetime of the bullet, in seconds
-        self.__speed = 500                      # Speed of the bullet, in pixels/sec
-        self.origin = origin                    # Robot from which this bullet was created
+
+        self.__lifetime = 2     # Lifetime of the bullet (seconds)
+        self.__speed = 500      # Speed of the bullet (pixels/second)
+        self.origin = origin    # Robot from which this bullet was created
 
         # Add origin to collision filter
         self.collision_filter.add(origin)
@@ -35,11 +39,11 @@ class Bullet(entity.Entity):
 
     @property
     def hitbox(self) -> list[Vector2]:
-        return [
-            Vector2(BULLET_RADIUS, 0).rotate_rad(i * 2 * math.pi / NUM_HITBOX_VERTICES)
-            for i in range(NUM_HITBOX_VERTICES)
-        ]
+        dangle = 2 * math.pi / NUM_HITBOX_VERTICES
+        return [Vector2(BULLET_RADIUS, 0).rotate_rad(i * dangle)
+                for i in range(NUM_HITBOX_VERTICES)]
 
+    # Override collisions with our own displacement logic
     @property
     def reacts_to_collisions(self) -> bool:
         return False
@@ -52,22 +56,26 @@ class Bullet(entity.Entity):
             other.destroy()
             self.destroy()
         elif type(other) is entity.Wall:
-            # Fix collision
+            # React to collision
             self.position += translation
 
-            # If the dot product is positive, then the normal is in the same direction as movement, so we don't reflect
+            # If the dot product is positive, then the normal is in the same
+            # direction as movement, so we don't reflect
             if Vector2(1, 0).rotate_rad(self.rotation).dot(translation) > 0:
                 return
 
             # Add collision point to path
             self.__path.append(self.position)
 
-            direction = Vector2(1, 0).rotate_rad(self.rotation).reflect(translation.normalize())
+            normal = translation.normalize()
+
+            direction = Vector2(1, 0).rotate_rad(self.rotation).reflect(normal)
             self.rotation = math.atan2(direction.y, direction.x)
 
     def __compute_trail_vertices(self) -> list[Vector2]:
         """
-        Computes vertices of the drawn trail and prunes old vertices from Bullet.__path
+        Computes vertices of the drawn trail and prunes old vertices from
+        Bullet.__path
         """
         vertices = [self.position]
         total_distance = 0
@@ -79,8 +87,10 @@ class Bullet(entity.Entity):
             distance = (vertex1 - vertex2).magnitude()
 
             if total_distance + distance >= TRAIL_LENGTH:
-                # Insert a vertex between vertex1 and vertex2 that caps length at TRAIL_LENGTH
-                vertices.append(vertex1.lerp(vertex2, (TRAIL_LENGTH - total_distance) / distance))
+                # Insert a vertex between vertex1 and vertex2 that caps length
+                # at TRAIL_LENGTH
+                alpha = (TRAIL_LENGTH - total_distance) / distance
+                vertices.append(vertex1.lerp(vertex2, alpha))
 
                 # Remove old vertices from self.__path that are too far away
                 self.__path = self.__path[-i - 1:]
@@ -92,9 +102,8 @@ class Bullet(entity.Entity):
         return vertices
 
     def update(self, dt: float):
-        assert self.arena is not None, "Bullet does not have a corresponding Arena"
-
-        self.position += Vector2(self.__speed * dt, 0).rotate_rad(self.rotation)
+        displacement = self.__speed * dt
+        self.position += Vector2(displacement, 0).rotate_rad(self.rotation)
         self.__lifetime -= dt
 
         if self.__lifetime < 0:
@@ -103,10 +112,8 @@ class Bullet(entity.Entity):
 
     def render(self, screen: pygame.Surface):
         # Draw trail
-        draw_gradient_path(
-            screen, TRAIL_HEAD_COLOR, TRAIL_TAIL_COLOR, self.__compute_trail_vertices(),
-            BULLET_RADIUS * 2
-        )
+        draw_gradient_path(screen, TRAIL_HEAD_COLOR, TRAIL_TAIL_COLOR,
+                           self.__compute_trail_vertices(), BULLET_RADIUS * 2)
 
         # Draw bullet circle
         pygame.draw.circle(screen, BULLET_COLOR, self.position, BULLET_RADIUS)
