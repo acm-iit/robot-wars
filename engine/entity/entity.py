3c50469bcf924305043df3583c7c015eb3fa9e96
Tommy Vadakumchery <t.vadakumchery1@gmail.com>
2023-04-03 21:46:54 -0500

    Address linting issues in engine/entity/entity.py

--- engine/entity/entity.py
+++ engine/entity/entity.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 import math
-import pygame
 from typing import Optional
+
+import pygame
 
 import engine.arena as arena
 from engine.util import check_polygon_collision, get_minimum_translation_vector
@@ -9,22 +10,22 @@ from engine.util import check_polygon_collision, get_minimum_translation_vector
 Rect = pygame.Rect
 Vector2 = pygame.Vector2
 
+
 class Entity:
     """
-    Object that can be rendered within an Arena.
-    This is an abstract class for common entity behaviors.
-    It is not meant to be instantiated in practice.
+    Object that can be rendered within an Arena. This is an abstract class for
+    common entity behaviors. It is not meant to be instantiated in practice.
     Other classes, like the Robot, Bullet, and Wall, inherit this class.
     """
     def __init__(self):
         self.position = Vector2()
         self.rotation = 0
-        self.arena: Optional[arena.Arena] = None        # Arena containing this entity
-        self.collision_filter: set[Entity] = set()      # Don't collide with entities in this set
+        self.arena: Optional[arena.Arena] = None        # Containing Arena
+        self.collision_filter: set[Entity] = set()      # No-collide set
 
-    # Separate vector attributes into getter/setter to force copy on assign. Otherwise,
-    # one could assign a position to an entity and then update that entity's position
-    # via the original vector value!
+    # Separate vector attributes into getter/setter to force copy on assign.
+    # Otherwise, one could assign a position to an entity and then update that
+    # entity's position via the original vector value!
     @property
     def position(self) -> Vector2:
         return self.__position.copy()
@@ -45,83 +46,78 @@ class Entity:
     @property
     def hitbox(self) -> list[Vector2]:
         """
-        Hitbox of this entity, as a list of vertices in order (without position and rotation offset).
+        Hitbox of this entity, as a list of vertices in order (without position
+        and rotation offset).
         """
         return [Vector2(1, 1), Vector2(1, -1), Vector2(-1, -1), Vector2(-1, 1)]
 
     @property
     def absolute_hitbox(self) -> list[Vector2]:
-        """
-        Absolute positions of entity hitbox vertices, in order.
-        """
-        return [vertex.rotate_rad(self.rotation) + self.position for vertex in self.hitbox]
+        """Absolute positions of entity hitbox vertices, in order."""
+        return [vertex.rotate_rad(self.rotation) + self.position
+                for vertex in self.hitbox]
 
     @property
     def rect(self) -> Rect:
-        """
-        Axis-aligned bounding rectangle of the entity.
-        """
-        absolute_hitbox = self.absolute_hitbox
-        top_left = Vector2(min(point.x for point in absolute_hitbox), min(point.y for point in absolute_hitbox))
-        bottom_right = Vector2(max(point.x for point in absolute_hitbox), max(point.y for point in absolute_hitbox))
-        return Rect(top_left, bottom_right - top_left)
+        """Axis-aligned bounding rectangle of the entity."""
+        min_x, min_y = math.inf, math.inf
+        max_x, max_y = -math.inf, -math.inf
+
+        for point in self.absolute_hitbox:
+            min_x = min(min_x, point.x)
+            min_y = min(min_y, point.y)
+            max_x = max(max_x, point.x)
+            max_y = max(max_y, point.y)
+
+        return Rect(Vector2(min_x, min_y),
+                    Vector2(max_x - min_x, max_y - min_y))
 
     @property
     def is_static(self) -> bool:
-        """
-        If this entity is static, it cannot move or be displaced.
-        """
+        """If this entity is static, it cannot move or be displaced."""
         return False
 
     @property
     def reacts_to_collisions(self) -> bool:
         """
-        If `False`, then the physics reaction to collision will be omitted.
-        The `on_collide` method will still be called.
+        If `False`, then the physics reaction to collision will be omitted. The
+        `on_collide` method will still be called.
         """
         return True
 
     def update(self, dt: float):
-        """
-        Update the entity's state after a time delta `dt`, in seconds.
-        """
+        """Update the entity's state after a time delta `dt`, in seconds."""
         pass
 
     def render(self, screen: pygame.Surface):
-        """
-        Renders the entity onto a screen.
-        """
+        """Renders the entity onto a screen."""
         pass
 
     def destroy(self):
-        """
-        Removes the entity from its parent arena.
-        """
-        # TODO: Commented out the below assert temporarily, since this method is idempotent.
-        # If this changes in the future, we need to uncomment this assert and fix cases where
-        # it is triggered.
+        """Removes the entity from its parent arena."""
+        # TODO: Commented out the below assert temporarily, since this method
+        # is idempotent. If this changes in the future, we need to uncomment
+        # this assert and fix cases where it is triggered.
         # assert self.arena is not None, "Entity has already been destroyed"
         self.arena = None
 
     def on_collide(self, other: Entity, translation: Vector2):
-        """
-        Called when this entity collides with another entity.
-        """
+        """Called when this entity collides with another entity."""
         pass
-        
+
     def is_colliding_with(self, other: Entity) -> tuple[bool, Vector2]:
-        """
-        Determines if this entity is colliding with another entity.
-        """
+        """Determines if this entity is colliding with another entity."""
         if other in self.collision_filter or self in other.collision_filter:
             return False, Vector2()
-        
+
         self_hitbox = self.absolute_hitbox
         other_hitbox = other.absolute_hitbox
 
         is_colliding = check_polygon_collision(self_hitbox, other_hitbox)
         if is_colliding:
-            return True, get_minimum_translation_vector(self_hitbox, other_hitbox)
+            translation = get_minimum_translation_vector(self_hitbox,
+                                                         other_hitbox)
+            return True, translation
         else:
             return False, Vector2()
 
@@ -129,7 +125,7 @@ class Entity:
         """
         Handles potential collision between this entity and another entity.
         """
-        # If both entities are static then collisions don't matter
+        # If both entities are static, then collisions don't matter
         if self.is_static and other.is_static:
             return
 
@@ -142,7 +138,8 @@ class Entity:
         self.on_collide(other, translation)
         other.on_collide(self, -translation)
 
-        # If one or more of the entities has react_to_collisions = False, then skip
+        # If one or more of the entities has react_to_collisions = False, then
+        # don't displace them
         if not self.reacts_to_collisions or not other.reacts_to_collisions:
             return
 
