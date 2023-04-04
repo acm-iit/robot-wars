b8542bba9842fd747a17c15977070d25ad73db42
Tommy Vadakumchery <t.vadakumchery1@gmail.com>
2023-04-03 20:05:27 -0500

    Use single flake8: noqa in engine/__init__.py

--- engine/__init__.py
+++ engine/__init__.py
@@ -1,8 +1,9 @@
+# flake8: noqa
 import os
 os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"
 
-from engine.arena import Arena as Arena         # noqa
-from engine.entity import Bullet as Bullet      # noqa
-from engine.entity import Entity as Entity      # noqa
-from engine.entity import Robot as Robot        # noqa
-from engine.entity import Wall as Wall          # noqa
+from engine.arena import Arena as Arena
+from engine.entity import Bullet as Bullet
+from engine.entity import Entity as Entity
+from engine.entity import Robot as Robot
+from engine.entity import Wall as Wall
