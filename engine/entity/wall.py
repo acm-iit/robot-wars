e57763ece12e2eb3e02641727014d65b15abd71e
Tommy Vadakumchery <t.vadakumchery1@gmail.com>
2023-04-17 06:54:16 -0500

    Define Wall.pathfinding_hitbox

--- engine/entity/wall.py
+++ engine/entity/wall.py
@@ -1,6 +1,7 @@
 import pygame
 
 import engine.entity as entity
+from engine.entity.robot import ROBOT_HITBOX_WIDTH
 
 Vector2 = pygame.Vector2
 
@@ -22,6 +23,25 @@ class Wall(entity.Entity):
             -half_size,
             Vector2(-half_size.x, half_size.y)
         ]
+
+    @property
+    def pathfinding_hitbox(self) -> list[Vector2]:
+        """
+        Hitbox of the Wall expanded by the Robot agent radius, in absolute
+        coordinates.
+        """
+        half_size = self.__size / 2
+        half_size_reflect = Vector2(half_size.x, -half_size.y)
+        robot_size = Vector2(ROBOT_HITBOX_WIDTH + 2) / 2
+        robot_size_reflect = Vector2(robot_size.x, -robot_size.y)
+        expanded = [
+            half_size + robot_size,
+            half_size_reflect + robot_size_reflect,
+            -half_size - robot_size,
+            -half_size_reflect - robot_size_reflect
+        ]
+        return [vertex.rotate_rad(self.rotation) + self.position
+                for vertex in expanded]
 
     @property
     def is_static(self) -> bool:
