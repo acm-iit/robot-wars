ef90a3589466572000c71917a56517e71f0ce37a
Tommy Vadakumchery <t.vadakumchery1@gmail.com>
2023-04-21 09:34:25 -0500

    Change docstrings in control.py to use 'tank' instead of 'robot'

--- engine/control.py
+++ engine/control.py
@@ -22,15 +22,15 @@ def angle_to(x1: float, y1: float, x2: float, y2: float) -> float:
 
 class ControllerState:
     """
-    Object that resembles the current state of the robot. The values in this
-    object can be used to determine how the robot should act in the upcoming
+    Object that resembles the current state of the tank. The values in this
+    object can be used to determine how the tank should act in the upcoming
     time step, which is written into the ControllerAction.
     """
     def __init__(self):
         self.time_delta: float = 0
         """
         Elapsed time that this time step will simulate, in seconds. This is
-        helpful for precise physics calculations, like predicting robot or
+        helpful for precise physics calculations, like predicting tank or
         bullet paths!
         """
 
@@ -59,19 +59,19 @@ class ControllerState:
 
         self.position: tuple[float, float] = (0, 0)
         """
-        Position of the robot, as a tuple `(x, y)` measured from the top-left
+        Position of the tank, as a tuple `(x, y)` measured from the top-left
         point of the arena.
         """
 
         self.max_speed: float = 0
         """
-        Maximum movement speed for the robot; i.e. when `move_power = 1` or
+        Maximum movement speed for the tank; i.e. when `move_power = 1` or
         `-1`.
         """
 
         self.rotation: float = 0
         """
-        Rotation of the robot, in radians measured clockwise from the positive
+        Rotation of the tank, in radians measured clockwise from the positive
         X-axis (right).
 
         (It is measured clockwise since the positive Y-axis is down!)
@@ -79,13 +79,12 @@ class ControllerState:
 
         self.max_turn_speed: float = 0
         """
-        Maximum turning speed for the robot; i.e. when `turn_power = 1` or
-        `-1`.
+        Maximum turning speed for the tank; i.e. when `turn_power = 1` or `-1`.
         """
 
         self.turret_rotation: float = 0
         """
-        Rotation of the robot's turret, in radians measured clockwise from the
+        Rotation of the tank's turret, in radians measured clockwise from the
         positive X-axis (right).
 
         (It is measured clockwise since the positive Y-axis is down!)
@@ -93,13 +92,13 @@ class ControllerState:
 
         self.max_turret_turn_speed: float = 0
         """
-        Maximum turning speed for the robot's turret; i.e. when
+        Maximum turning speed for the tank's turret; i.e. when
         `turret_turn_power = 1` or `-1`.
         """
 
         self.shot_cooldown: float = 0
         """
-        Time left until the robot can shoot a bullet. Every time a robot shoots
+        Time left until the tank can shoot a bullet. Every time a tank shoots
         a bullet, it cannot shoot again for `1` second.
 
         If the value is `0`, then setting `ControllerAction.shoot = True` will
@@ -108,7 +107,7 @@ class ControllerState:
 
         self.shot_speed: float = 0
         """
-        Scalar speed of the robot's bullets when it shoots. Useful for
+        Scalar speed of the tank's bullets when it shoots. Useful for
         predicting own bullet paths.
         """
 
@@ -125,73 +124,73 @@ class ControllerState:
 
         self.enemy_position: tuple[float, float] = (0, 0)
         """
-        Position of the enemy robot, as a tuple `(x, y)` measured from the
+        Position of the enemy tank, as a tuple `(x, y)` measured from the
         top-left point of the arena.
 
-        In the case of multiple enemy robots (i.e. battle royale), this is the
-        position of the nearest enemy robot.
+        In the case of multiple enemy tanks (i.e. battle royale), this is the
+        position of the nearest enemy tank.
         """
 
         self.enemy_velocity: tuple[float, float] = (0, 0)
         """
-        Velocity of the enemy robot from the previous time step, as a tuple
+        Velocity of the enemy tank from the previous time step, as a tuple
         `(vx, vy)` measured from the top-left point of the arena.
 
-        In the case of multiple enemy robots (i.e. battle royale), this is the
-        velocity of the nearest enemy robot.
+        In the case of multiple enemy tanks (i.e. battle royale), this is the
+        velocity of the nearest enemy tank.
 
         Along with `time_delta`, this is useful for predicting enemy paths!
         """
 
         self.enemy_rotation: float = 0
         """
-        Rotation of the enemy robot, in radians measured clockwise from the
+        Rotation of the enemy tank, in radians measured clockwise from the
         positive X-axis (right).
 
-        In the case of multiple enemy robots (i.e. battle royale), this is the
-        rotation of the nearest enemy robot.
+        In the case of multiple enemy tanks (i.e. battle royale), this is the
+        rotation of the nearest enemy tank.
 
         (It is measured clockwise since the positive Y-axis is down!)
         """
 
         self.enemy_turret_rotation: float = 0
         """
-        Rotation of the enemy robot's turret, in radians measured clockwise
-        from the positive X-axis (right).
+        Rotation of the enemy tank's turret, in radians measured clockwise from
+        the positive X-axis (right).
 
-        In the case of multiple enemy robots (i.e. battle royale), this is the
-        rotation of the nearest enemy robot's turret.
+        In the case of multiple enemy tanks (i.e. battle royale), this is the
+        rotation of the nearest enemy tank's turret.
 
         (It is measured clockwise since the positive Y-axis is down!)
         """
 
         self.enemy_shot_cooldown: float = 0
         """
-        Time left until the enemy robot can shoot a bullet. Every time a robot
+        Time left until the enemy tank can shoot a bullet. Every time a tank
         shoots a bullet, it cannot shoot again for `1` second.
 
-        If the value is `0`, then the enemy robot is able to shoot a bullet.
+        If the value is `0`, then the enemy tank is able to shoot a bullet.
 
-        In the case of multiple enemy robots (i.e. battle royale), this is the
-        shot cooldown of the nearest enemy robot.
+        In the case of multiple enemy tanks (i.e. battle royale), this is the
+        shot cooldown of the nearest enemy tank.
         """
 
         self.can_see_enemy: bool = False
         """
-        Determines if the robot can see the enemy robot, as a boolean where
+        Determines if the tank can see the enemy tank, as a boolean where
         `True` implies it can do so.
 
         "See" is defined as having line-of-sight with the enemy, with no
         impeding obstacles.
 
-        In the case of multiple enemy robots (i.e. battle royale), this applies
-        to the nearest enemy robot.
+        In the case of multiple enemy tanks (i.e. battle royale), this applies
+        to the nearest enemy tank.
         """
 
         self.bullets: list[tuple[float, float, float, float]] = []
         """
         List of positions and velocities of enemy bullets in proximity to the
-        robot, as tuples `(x, y, vx, vy)`, where `x` and `y` are the positional
+        tank, as tuples `(x, y, vx, vy)`, where `x` and `y` are the positional
         coordinates measured from the top-left point of the arena, and `vx` and
         `vy` are the velocity components.
         """
@@ -205,13 +204,13 @@ class ControllerState:
 
 class ControllerAction:
     """
-    Object the represents the actions the robot will take in the upcoming time
-    step. Write into the values of this object to dictate the robot's behavior.
+    Object the represents the actions the tank will take in the upcoming time
+    step. Write into the values of this object to dictate the tank's behavior.
     """
     def __init__(self):
         self.move_power: float = 0
         """
-        Current velocity of the robot, represented as a fraction of maximum
+        Current velocity of the tank, represented as a fraction of maximum
         movement speed between `-1` and `1`, inclusive.
 
         Positive values correspond to moving forwards, while negative values
@@ -220,7 +219,7 @@ class ControllerAction:
 
         self.turn_power: float = 0
         """
-        Current angular velocity of the robot, represented as a fraction of
+        Current angular velocity of the tank, represented as a fraction of
         maximum turning speed between `-1` and `1`, inclusive.
 
         Positive values correspond to turning clockwise, while negative values
@@ -242,8 +241,8 @@ class ControllerAction:
 
         self.move_toward: tuple[float, float] | None = None
         """
-        Position that the robot should move toward using pathfinding, as a
-        tuple `(x, y)` measured from the top-left point of the arena.
+        Position that the tank should move toward using pathfinding, as a tuple
+        `(x, y)` measured from the top-left point of the arena.
 
         This value overrides `move_power`, `turn_power`, and `turn_toward`.
 
@@ -252,7 +251,7 @@ class ControllerAction:
 
         self.turn_toward: float | tuple[float, float] | None = None
         """
-        Angle or position that the robot should turn itself toward.
+        Angle or position that the tank should turn itself toward.
 
         Angles should be in radians measured clockwise from the positive X-axis
         (right), while positions should be tuples of floats `(x, y)` measured
@@ -267,7 +266,7 @@ class ControllerAction:
 
         self.aim_toward: float | tuple[float, float] | None = None
         """
-        Angle or position that the robot should aim its turret toward.
+        Angle or position that the tank should aim its turret toward.
 
         Angles should be in radians measured clockwise from the positive X-axis
         (right), while positions should be tuples of floats `(x, y)` measured
@@ -282,7 +281,7 @@ class ControllerAction:
 
         self.shoot: bool = False
         """
-        Determines if the robot should shoot during this time step.
+        Determines if the tank should shoot during this time step.
 
         `False` represents no shooting, `True` represents shooting.
         """
@@ -290,31 +289,31 @@ class ControllerAction:
 
 class Controller:
     """
-    Abstract robot controller class.
+    Abstract tank controller class.
 
     Do not modify this class; instead create a subclass of this class to
-    represent your robot. Override the below methods in your subclass to
-    implement the behavior of your robot.
+    represent your tank. Override the below methods in your subclass to
+    implement the behavior of your tank.
     """
-    name = "Robot"
+    name = "Tank"
     """
-    Name used to distinguish this robot between others in the leaderboard.
+    Name used to distinguish this tank between others in the leaderboard.
 
-    Change this value to name your robot.
+    Change this value to name your tank.
     """
 
     body_color = "#EEEEEE"
     """
-    Hexadecimal RGB color of the robot's body (the rectangle chassis).
+    Hexadecimal RGB color of the tank's body (the rectangle chassis).
 
-    Change this value to personalize your robot.
+    Change this value to personalize your tank.
     """
 
     head_color = "#CCCCCC"
     """
-    Hexadecimal RGB color of the robot's head (the circle in the middle).
+    Hexadecimal RGB color of the tank's head (the circle in the middle).
 
-    Change this value to personalize your robot.
+    Change this value to personalize your tank.
     """
 
     def __init__(self):
@@ -322,14 +321,14 @@ class Controller:
         Initializes the controller.
 
         Override this method if you want to add any extra persistent state that
-        you may need for your robot.
+        you may need for your tank.
         """
         pass
 
     def act(self, state: ControllerState) -> ControllerAction:
         """
-        Runs this Controller on a robot's state to produce an action that
-        determines how the robot will behave in this time step.
+        Runs this Controller on a tank's state to produce an action that
+        determines how the tank will behave in this time step.
 
         Override this with your own behavior!
         """
